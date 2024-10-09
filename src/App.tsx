6bd0345c70f5f57abb0d089979f5f9cfc6a40985
Guiton1er <bahiergael@gmail.com>
2024-10-09 12:09:45 +0200

    suppresed usecallback from imports 'cause it's useless (and not usecallback you see ?!HAHA)

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState, useCallback, useEffect } from 'react'
+import { useState, useEffect } from 'react'
 import capybara from './assets/capybara.png'
 import './App.css'
 import 'react-big-calendar/lib/css/react-big-calendar.css'
